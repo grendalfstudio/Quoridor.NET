c9b6bceeea021fe168a94a1ef47d8b891427e836
AndrryArmor <andrry1@gmail.com>
2021-10-07 04:28:57 +0300

    Added GameField and its dependent classes WallCenter and WallType

---
+++ Model/WallCenter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Model
+{
+    public struct WallCenter
+    {
+        public WallCenter(int northRow, int westColumn)
+        {
+            NorthRow = northRow;
+            WestColumn = westColumn;
+        }
+
+        public int NorthRow { get; }
+        public int WestColumn { get; }
+    }
+}
