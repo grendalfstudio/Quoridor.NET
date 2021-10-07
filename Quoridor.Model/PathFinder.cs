d5f36750f39ccbe7e3551193e5b27a970619a80b
AndrryArmor <andrry1@gmail.com>
2021-10-07 13:50:13 +0300

    Added PathFinder class and its dependent class WaveStep

---
+++ Quoridor.Model/PathFinder.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HavocAndCry.Quoridor.Model
+{
+    public class PathFinder
+    {
+        // Finds whether a path from player to his finish row exists using Lee (wave) algorithm
+        public static bool IsPathToExit(Player player, GameField gameField)
+        {
+            int[,] labyrinth = new int[gameField.Size, gameField.Size];
+            Queue<WaveStep> queue = new Queue<WaveStep>();
+            queue.Enqueue(new WaveStep(player.Row, player.Column, 1));
+
+            while (queue.Count > 0)
+            {
+                WaveStep waveStep = queue.Dequeue();
+
+                if (waveStep.Row < 0 || waveStep.Row >= gameField.Size || waveStep.Column < 0
+                    || waveStep.Column >= gameField.Size)
+                {
+                    continue;
+                }
+                else if (labyrinth[waveStep.Row, waveStep.Column] > 0)
+                {
+                    continue;
+                }
+
+                labyrinth[waveStep.Row, waveStep.Column] = waveStep.Step;
+                if (waveStep.Row == player.FinishRow)
+                {
+                    break;
+                }
+
+                var northWestWallPoint = new WallCenter(waveStep.Row - 1, waveStep.Column - 1);
+                var northEastWallPoint = new WallCenter(waveStep.Row - 1, waveStep.Column);
+                var southWestWallPoint = new WallCenter(waveStep.Row, waveStep.Column - 1);
+                var southEastWallPoint = new WallCenter(waveStep.Row, waveStep.Column);
+
+                if (!gameField.IsHorizontalWallAt(northWestWallPoint)
+                    && !gameField.IsHorizontalWallAt(northEastWallPoint))
+                {
+                    queue.Enqueue(new WaveStep(waveStep.Row - 1, waveStep.Column, waveStep.Step + 1));
+                }
+                if (!gameField.IsHorizontalWallAt(southWestWallPoint)
+                    && !gameField.IsHorizontalWallAt(southEastWallPoint))
+                {
+                    queue.Enqueue(new WaveStep(waveStep.Row + 1, waveStep.Column, waveStep.Step + 1));
+                }
+                if (!gameField.IsVerticalWallAt(northWestWallPoint)
+                    && !gameField.IsVerticalWallAt(southWestWallPoint))
+                {
+                    queue.Enqueue(new WaveStep(waveStep.Row, waveStep.Column - 1, waveStep.Step + 1));
+                }
+                if (!gameField.IsVerticalWallAt(northEastWallPoint)
+                    && !gameField.IsVerticalWallAt(southEastWallPoint))
+                {
+                    queue.Enqueue(new WaveStep(waveStep.Row, waveStep.Column + 1, waveStep.Step + 1));
+                }
+            }
+
+            for (int i = 0; i < gameField.Size; i++)
+            {
+                if (labyrinth[player.FinishRow, i] > 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Quoridor.Model/WaveStep.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HavocAndCry.Quoridor.Model
+{
+    public class WaveStep
+    {
+        public WaveStep(int row, int column, int step)
+        {
+            Row = row;
+            Column = column;
+            Step = step;
+        }
+
+        public int Row { get; }
+        public int Column { get; }
+        public int Step { get; }
+    }
+}
