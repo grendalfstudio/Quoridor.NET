64985ea8956d38faa14364e819734e3bee0d5f00
AndrryArmor <andrry1@gmail.com>
2021-10-07 04:12:11 +0300

    Added new project Model with class Player

---
+++ Model/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Model
+{
+    public class Player
+    {
+        public Player(int row, int column, int finishRow)
+        {
+            Row = row;
+            Column = column;
+            FinishRow = finishRow;
+        }
+
+        public int Row { get; private set; }
+        public int Column { get; private set; }
+        public int FinishRow { get; }
+
+        public void MovePlayer(int newRow, int newColumn)
+        {
+            Row = newRow;
+            Column = newColumn;
+        }
+    }
+}
