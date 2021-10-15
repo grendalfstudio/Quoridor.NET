51952a4369d28c01d5d969f58f42c348384c057a
Kolia Vaskevych <kolyavaskevich19@gmail.com>
2021-10-15 14:28:48 +0300

    Updated ConsoleClient project to .net 6

--- Quoridor.ConsoleClient/Program.cs
+++ Quoridor.ConsoleClient/Program.cs
@@ -1,22 +1,11 @@
 ﻿using System;
 using HavocAndCry.Quoridor.ConsoleClient.Controller;
-using HavocAndCry.Quoridor.ConsoleClient.Models;
-using HavocAndCry.Quoridor.Core.Models;
 
-namespace HavocAndCry.Quoridor.ConsoleClient
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            GameController game = new();
-            game.StartGame();
+var game = new GameController();
+game.StartGame();
 
-            // var fieldViewModel = new GameFieldViewModel(2);
-            // fieldViewModel.UpdateFieldView(new GameField(2));
-            // fieldViewModel.PrintField();
-            
-            Console.ReadKey();
-        }
-    }
-}
+// var fieldViewModel = new GameFieldViewModel(2);
+// fieldViewModel.UpdateFieldView(new GameField(2));
+// fieldViewModel.PrintField();
+
+Console.ReadKey();
