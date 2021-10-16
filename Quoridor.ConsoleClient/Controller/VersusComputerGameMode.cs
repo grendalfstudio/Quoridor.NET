f6c8db2313c84c03b1c3f3e3450cb5fda1a939b7
AndrryArmor <andrry1@gmail.com>
2021-10-16 22:26:16 +0300

    Added random moves generation for computer player

--- Quoridor.ConsoleClient/Controller/VersusComputerGameMode.cs
+++ Quoridor.ConsoleClient/Controller/VersusComputerGameMode.cs
@@ -1,11 +1,18 @@
 ﻿using HavocAndCry.Quoridor.ConsoleClient.Abstract;
+using HavocAndCry.Quoridor.ConsoleClient.Models;
+using HavocAndCry.Quoridor.Core.Models;
+using HavocAndCry.Quoridor.Model.Services;
+using System;
 
 namespace HavocAndCry.Quoridor.ConsoleClient.Controller
 {
     public class VersusComputerGameMode : AbstractGameMode
     {
+        private readonly Random _random;
+
         public VersusComputerGameMode() : base(2)
         {
+            _random = new Random();
         }
 
         public override void StartMainCycle()
@@ -32,9 +39,43 @@ namespace HavocAndCry.Quoridor.ConsoleClient.Controller
         }
 
         private void MakeBotTurn()
+{
+            var turn = (TurnMenuOptions)_random.Next(1, 3);
+            switch (turn)
+            {
+                case TurnMenuOptions.Move:
+                    MakeMove();
+                    break;
+                case TurnMenuOptions.SetWall:
+                    SetWall();
+                    break;
+            }
+        }
+
+        protected new void MakeMove()
         {
-            _consoleView.WriteLine("Bot's turn");
-            _consoleView.Redraw();
+            var possibleMoves = _turnService.GetPossibleMoves(_currentPlayerId);
+            int randomIndex = _random.Next(possibleMoves.Count);
+            _turnService.TryMove(possibleMoves[randomIndex], _currentPlayerId);
+            _consoleView.SetFieldChanged();
+        }
+
+        protected new void SetWall()
+        {
+            int randomRow;
+            int randomColumn;
+            WallType randomWallType;
+            Wall randomWall;
+            do
+            {
+                randomRow = _random.Next(0, 8);
+                randomColumn = _random.Next(0, 8);
+
+                randomWallType = _random.NextDouble() >= 0.5 ? WallType.Horizontal : WallType.Vertical;
+                randomWall = new Wall(randomWallType, new WallCenter(randomRow, randomColumn));
+            } while (!_turnService.TrySetWall(randomWall, _currentPlayerId));
+
+            _consoleView.SetFieldChanged();
         }
     }
 }
