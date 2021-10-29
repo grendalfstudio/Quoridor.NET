1487230bb976f6c4111216e414e1e88e4f86a7e2
AndrryArmor <andrry1@gmail.com>
2021-10-29 04:07:30 +0300

    Added nary tree

---
+++ Quoridor.Bot/MinimaxNode.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quoridor.Bot
+{
+    public class MinimaxNode
+    {
+        private readonly List<MinimaxNode> _children;
+
+        public MinimaxNode(MinimaxNode parent, string moveRepresentation)
+        {
+            Parent = parent;
+            MoveRepresentation = moveRepresentation;
+            _children = new List<MinimaxNode>();
+        }
+
+        public MinimaxNode Parent { get; }
+        public IReadOnlyList<MinimaxNode> Children => _children;
+        public string MoveRepresentation { get; }
+
+        public void AddChildrenNodes(List<string> moves)
+        {
+            var childNodes = moves.Select(move => new MinimaxNode(this, move));
+            _children.AddRange(childNodes);
+        }
+
+        public int EvaluateScore()
+        {
+            return _children.Count == 0
+                ? 1 
+                : _children.Sum(child => child.EvaluateScore());
+        }
+    }
+}
--- Quoridor.ConsoleClient/Program.cs
+++ Quoridor.ConsoleClient/Program.cs
@@ -1,7 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 using HavocAndCry.Quoridor.ConsoleClient.Controller;
+using Quoridor.Bot;
 
-var game = new GameController();
-game.StartGame();
+//var game = new GameController();
+//game.StartGame();
+
+var minimaxTree = new MinimaxTree();
+minimaxTree.AddChildrenNodes(new List<string> { "B4", "X3h", "A3", "Z7v" });
+minimaxTree.Children[0].AddChildrenNodes(new List<string> { "B4", "X3h", "A3", "Z7v" });
+Console.WriteLine(minimaxTree.EvaluateScore());
 
 Console.ReadKey();
